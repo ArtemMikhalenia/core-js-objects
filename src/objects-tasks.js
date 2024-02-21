3310b7ae10e27660c2f52117b8266f0ceb1488f6
Artem <goldtree3000@gmail.com>
2024-02-21 15:59:09 +0300

    feat: solve makeWord task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -135,8 +135,19 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const resultLength = Math.max(...[].concat(...Object.values(lettersObject)));
+  const result = new Array(resultLength > 0 ? resultLength + 1 : 1).fill('');
+
+  Object.entries(lettersObject).forEach(([key, value]) => {
+    value.forEach(() => {
+      for (let i = 0; i < value.length; i += 1) {
+        result.splice(value[i], 1, key);
+      }
+    });
+  });
+
+  return result.join('');
 }
 
 /**
