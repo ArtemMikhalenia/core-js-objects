2cc83361df4d970fbc665717209a21a742ce17ff
Artem <goldtree3000@gmail.com>
2024-02-27 13:59:59 +0300

    feat: solve cssSelectorBuilder task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -366,32 +366,68 @@ function group(array, keySelector, valueSelector) {
  */
 
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  selectors: '',
+  orderArray: [1, 2, 6],
+
+  createNewObj(value, order) {
+    const obj = Object.create(this);
+    obj.order = order;
+    obj.selectors = this.selectors + value;
+    return obj;
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  element(value) {
+    this.checkOrder(1);
+    return this.createNewObj(value, 1);
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    this.checkOrder(2);
+    return this.createNewObj(`#${value}`, 2);
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    this.checkOrder(3);
+    return this.createNewObj(`.${value}`, 3);
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    this.checkOrder(4);
+    return this.createNewObj(`[${value}]`, 4);
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    this.checkOrder(5);
+    return this.createNewObj(`:${value}`, 5);
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    this.checkOrder(6);
+    return this.createNewObj(`::${value}`, 6);
+  },
+
+  combine(selector1, combinator, selector2) {
+    const obj = Object.create(this);
+    obj.selectors = `${selector1.selectors} ${combinator} ${selector2.selectors}`;
+    return obj;
+  },
+
+  checkOrder(num) {
+    if (this.order === num && this.orderArray.includes(num)) {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more then one time inside the selector'
+      );
+    }
+
+    if (this.order > num) {
+      throw new Error(
+        'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element'
+      );
+    }
+  },
+
+  stringify() {
+    return this.selectors;
   },
 };
 
